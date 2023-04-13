bc0dd05b99a87fa068749ba0e9228c34836d2cbc
pau4488 <paubrossa@gmail.com>
2023-04-13 20:17:22 +0200

    rev

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -1,14 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 18:05:18 by marvin            #+#    #+#             */
+/*   Updated: 2023/04/13 18:05:18 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
 int	ft_strlen(char *str);
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(char *s)
 {
 	char	*cpy;
 
-	cpy = malloc(ft_strlen(str) * sizeof(char));
-	if(!cpy)
-		return(0);
-	cpy = str;
-	return(cpy);
+	cpy = malloc(ft_strlen(s) * sizeof(char));
+	if (!cpy)
+		return (0);
+	cpy = s;
+	return (cpy);
 }
 
 int	ft_strlen(char *str)
@@ -16,7 +30,7 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
-	while(str)
+	while (*str)
 	{
 		i++;
 		str++;
--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -1,17 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 18:34:05 by marvin            #+#    #+#             */
+/*   Updated: 2023/04/13 18:34:05 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
 int	*ft_range(int min, int max)
 {
 	int	*arr;
 	int	i;
 
 	i = 0;
-	arr = 0;
-	if(min >= max)
+	arr = malloc((max - 1) * sizeof(int));
+	if (min >= max)
 		return(NULL);
-	while(min < max)
+	while (min < max)
 	{
 		arr[i] = min;
 		i++;
 		min++;
 	}
-	return(arr);
+	return (arr);
 }
+
